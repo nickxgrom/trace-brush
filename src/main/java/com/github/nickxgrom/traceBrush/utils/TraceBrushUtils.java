d09d633f0a6eeb198e4c2927a0267a794f467e2c
nickxgrom <nxgr.dev@gmail.com>
2025-09-12 23:12:20 +0500

    Enhance TraceBrushUtils to check for TraceBrush item in hand using persistent data

--- src/main/java/com/github/nickxgrom/traceBrush/utils/TraceBrushUtils.java
+++ src/main/java/com/github/nickxgrom/traceBrush/utils/TraceBrushUtils.java
@@ -1,12 +1,26 @@
 package com.github.nickxgrom.traceBrush.utils;
 
+import com.github.nickxgrom.traceBrush.TraceBrush;
 import com.github.nickxgrom.traceBrush.models.TraceBrushItem;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public class TraceBrushUtils {
+    private static final NamespacedKey key = new NamespacedKey(JavaPlugin.getPlugin(TraceBrush.class), "is_trace_brush");
+
     public static boolean isBrushInHand(@NotNull Player player) {
-        return player.getInventory().getItemInMainHand().getType() == TraceBrushItem.getTraceBrush().getType();
+        ItemStack itemInHand = player.getInventory().getItemInMainHand();
+        ItemMeta meta = itemInHand.getItemMeta();
+
+        if (meta == null) return false;
+
+        return itemInHand.getType() == TraceBrushItem.getTraceBrush().getType()
+                && Boolean.TRUE.equals(meta.getPersistentDataContainer().get(key, PersistentDataType.BOOLEAN));
     }
 
     public static int secondsToTicks(int seconds) {
