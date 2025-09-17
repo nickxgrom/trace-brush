897c02c77c1eb0e48ae5c001f71771b08c69e086
nickxgrom <nxgr.dev@gmail.com>
2025-09-17 19:59:46 +0500

    Rework UseTraceBrushOnBlock, replace TraceBrushItem base from recovery_compass to brush to get natural animation and sound

--- src/main/java/com/github/nickxgrom/traceBrush/listeners/UseTraceBrushOnBlock.java
+++ src/main/java/com/github/nickxgrom/traceBrush/listeners/UseTraceBrushOnBlock.java
@@ -15,7 +15,6 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 import static com.github.nickxgrom.traceBrush.utils.TraceBrushUtils.isBrushInHand;
@@ -25,57 +24,50 @@ public class UseTraceBrushOnBlock implements Listener {
     private final TraceBrush plugin = JavaPlugin.getPlugin(TraceBrush.class);
     private final int RUB_TIME_IN_SECONDS = plugin.getConfig().getInt("rubTimeInSeconds");
     private final int MAX_TARGET_DISTANCE = plugin.getConfig().getInt("maxTargetDistance");
-    private final long HOLD_TIMEOUT_MS = 200;
     private final CoreProtectAPI coreProtectAPI = new CoreProtectHook().getCoreProtectAPI();
 
 
     @EventHandler
     public void onBrushUse(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-
         if (!isBrushInHand(player) || TraceBrushUtils.isBrushHasFingerprint(player)) return;
         if (event.getClickedBlock() == null) return;
 
         UUID playerId = player.getUniqueId();
         Block targetBlock = event.getClickedBlock();
-        long now = System.currentTimeMillis();
 
-        if (plugin.activeBlockTraces.containsKey(playerId)) {
-            if (plugin.activeBlockTraces.get(playerId).equals(targetBlock.getLocation())) {
-                plugin.playersHoldingRightClickTimestamp.put(playerId, now);
-            }
-
-            return;
-        }
+        if (plugin.activeBlockTraces.containsKey(playerId)) return;
 
         plugin.activeBlockTraces.put(playerId, targetBlock.getLocation());
-        plugin.playersHoldingRightClickTimestamp.put(playerId, now);
 
         new BukkitRunnable() {
             final int maxTicks = secondsToTicks(RUB_TIME_IN_SECONDS);
             int ticks = 0;
+            Block currentTarget = targetBlock;
 
             @Override
             public void run() {
-                ticks++;
-
-                boolean isPlayerInProgress = plugin.activeBlockTraces.containsKey(playerId);
-                boolean isKeyPressed = plugin.playersHoldingRightClickTimestamp.get(playerId) != null && System.currentTimeMillis() - plugin.playersHoldingRightClickTimestamp.get(playerId) <= HOLD_TIMEOUT_MS;
-                boolean isLookingAtTarget;
-
-                try {
-                    isLookingAtTarget = Objects.requireNonNull(player.getTargetBlockExact(MAX_TARGET_DISTANCE)).getLocation().equals(targetBlock.getLocation());
-                } catch (Exception e) {
-                    isLookingAtTarget = false;
-                }
-
-                if (!isPlayerInProgress || !isKeyPressed || !isLookingAtTarget || !isBrushInHand(player)) {
+                if (!player.isHandRaised() || !isBrushInHand(player)) {
                     cleanup();
                     return;
                 }
 
+                Block targetBlockExact = player.getTargetBlockExact(MAX_TARGET_DISTANCE);
+
+                if (targetBlockExact == null) {
+                    cleanup();
+                    return;
+                }
+
+                if (!targetBlockExact.getLocation().equals(currentTarget.getLocation())) {
+                    currentTarget = targetBlockExact;
+                    ticks = 0;
+                }
+
+                ticks++;
+
                 if (ticks >= maxTicks) {
-                    List<String[]> lookup = coreProtectAPI.blockLookup(targetBlock, 0);
+                    List<String[]> lookup = coreProtectAPI.blockLookup(currentTarget, 0);
 
                     if (!lookup.isEmpty()) {
                         String placedBy = null;
@@ -93,7 +85,7 @@ public class UseTraceBrushOnBlock implements Listener {
                             return;
                         }
 
-                        TraceBrushItem.writeFingerprintToBrush(player, Bukkit.getOfflinePlayer(placedBy).getUniqueId(), targetBlock);
+                        TraceBrushItem.writeFingerprintToBrush(player, Bukkit.getOfflinePlayer(placedBy).getUniqueId(), currentTarget);
                     } else {
                         TraceBrushItem.writeFingerprintToBrush(player, null, null);
                     }
@@ -104,7 +96,6 @@ public class UseTraceBrushOnBlock implements Listener {
 
             private void cleanup() {
                 plugin.activeBlockTraces.remove(playerId);
-                plugin.playersHoldingRightClickTimestamp.remove(playerId);
                 cancel();
             }
         }.runTaskTimer(plugin, 0, 1);
